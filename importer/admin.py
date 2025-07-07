fbae3fa8fb4cf49701ae8e4769a2ac6927b27c2d
grant0711 <adamsgrant87@gmail.com>
2025-07-06 19:53:19 -0500

    Display calculated date and duration on list display based on trackpoints

--- importer/admin.py
+++ importer/admin.py
@@ -1,19 +1,27 @@
 from django.contrib import admin
 from django_admin_inline_paginator.admin import TabularInlinePaginated
+from django.db.models import Min, Max
 
 from .models import Hike, TrackPoint
 
 
 class TrackPointInline(TabularInlinePaginated):
     model = TrackPoint
-    fields = ('isof_datetime', 'lat', 'lon', 'heart_rate')
-    readonly_fields = ('isof_datetime', 'lat', 'lon', 'heart_rate')
     ordering = ('datetime',)
     per_page = 20
     can_delete = False
+    fields = ('isof_datetime', 'lat', 'lon', 'heart_rate')
+    extra = 0
+
+    def get_readonly_fields(self, request, obj=None):
+        return self.fields
+
+    def has_change_permission(self, request, obj=None):
+        return False
 
     def has_add_permission(self, request, obj=None):
         return False
+
 
     @admin.display(description='Datetime (ISO)', ordering='datetime')
     def isof_datetime(self, obj):
@@ -22,8 +30,29 @@ class TrackPointInline(TabularInlinePaginated):
         return None
 
 class HikeAdmin(admin.ModelAdmin):
-    list_display = ("name",)
+    list_display = ("name", "get_date", "get_duration")
     inlines = (TrackPointInline,)
     fields = ('name', 'description')
+
+    def get_queryset(self, request):
+        queryset = super().get_queryset(request)
+        queryset = queryset.annotate(
+            start_time=Min('points__datetime'),
+            end_time=Max('points__datetime')
+        )
+        return queryset
+
+    @admin.display(description='Date', ordering='start_time')
+    def get_date(self, obj):
+        if obj.start_time:
+            return obj.start_time.date()
+        return None
+
+    @admin.display(description='Duration (minutes)')
+    def get_duration(self, obj):
+        if obj.start_time and obj.end_time:
+            duration = obj.end_time - obj.start_time
+            return round(duration.total_seconds() / 60)
+        return None
 
 admin.site.register(Hike, HikeAdmin)
--- importer/models.py
+++ importer/models.py
@@ -5,19 +5,27 @@ from django.db import models
 def create_uuid():
     return uuid.uuid4()
 
+class HikeQuerySet(models.QuerySet):
+    def delete(self):
+        self.update(is_deleted=True)
+
 class HikeManager(models.Manager):
     def get_queryset(self):
-        return super().get_queryset().filter(is_deleted=False)
+        return HikeQuerySet(self.model, using=self._db).filter(is_deleted=False)
 
 class Hike(models.Model):
 
-    objects = HikeManager()
+    objects = HikeManager.from_queryset(HikeQuerySet)()
 
     id = models.UUIDField(primary_key=True, default=create_uuid)
     file_name = models.CharField(max_length=255, unique=True, db_index=True)
     is_deleted = models.BooleanField(default=False)
     name = models.CharField(max_length=50, null=True, blank=True)
     description = models.TextField(null=True, blank=True)
+
+    def delete(self, using=None, keep_parents=False):
+        self.is_deleted = True
+        self.save()
 
 class TrackPoint(models.Model):
     id = models.UUIDField(primary_key=True, default=create_uuid)
